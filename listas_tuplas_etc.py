dd5330c69c63a834bb2712ea398250fe49cbccd0
RosanaNei <rosageene@gmail.com>
2022-09-11 01:38:49 -0300

    guardando listas

--- listas_tuplas_etc.py
+++ listas_tuplas_etc.py
@@ -63,7 +63,10 @@ shopping.extend('limon')
 print(shopping)
 shopping.append('limon')
 print(shopping)
-#Borrar elementos. Python nos ofrece, al menos, cuatro formas para borrar elementos en una lista:
+
+#BORRAR ELEMENTOS. 
+# Python nos ofrece, al menos, cuatro formas para borrar elementos en una lista:
+
 # Por su índice:Mediante la función del():
 shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
 del(shopping[3])
@@ -72,6 +75,98 @@ print(shopping)
 shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
 shopping.remove('Huevos')
 print(shopping)
+#Por su índice (con extracción):
+shopping.pop(2)
+print(shopping)
+#Por su rango: Mediante troceado de listas:
+shopping[1:4] = []
+
+#BORRADO COMPLETO DE LA LISTA 
+# Utilizando la función clear():
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+shopping.clear()
+#Reinicializando» la lista a vacío con []:
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+shopping = []
+
+#Encontrar un elemento. Si queremos descubrir el índice que corresponde a un determinado valor dentro la lista podemos 
+# usar la función index() para ello:
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+print(shopping.index('Huevos')) 
+#si el elemento que buscamos no está en la lista, obtendremos un error. Y Si buscamos un valor que existe más de una 
+# vez en una lista, la función index() sólo nos devolverá el índice de la primera ocurrencia.
+
+#PERTENENCIA DE UN ELEMENTO: Si queremos comprobar la existencia de un determinado elemento en una lista, podríamos 
+# buscar su índice con el operador in:  El operador in siempre devuelve un valor booleano, es decir, verdadero o falso. 
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+'Aceite' in shopping
+'Pollo' in shopping
+
+#NUMERO DE OCURRENCIAS Para contar cuántas veces aparece un determinado valor dentro de una lista podemos usar la 
+# función count():
+sheldon_greeting = ['Penny', 'Penny', 'Penny']
+sheldon_greeting.count('Howard')
+sheldon_greeting.count('Penny')
+
+#Convertir lista a cadena de texto Dada una lista, podemos convetirla a una cadena de texto, uniendo todos sus 
+# elementos mediante algún separador. ATENCION!! join() sólo funciona si todos sus elementos son cadenas de texto
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+','.join(shopping)
+' '.join(shopping)
+'|'.join(shopping)
+
+#Ordenar una lista.dos formas de ordenar los elementos de una lista:
+# Conservando lista original:Mediante la función sorted() que devuelve una nueva lista ordenada:
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+sorted(shopping)
+
+#Modificando la lista original:Mediante la función sort():
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+shopping.sort()
+print(f'esta lista está modificada {shopping} ')
+#Ambos métodos admiten un parámetro «booleano» 
+# reverse para indicar si queremos que la ordenación se haga en sentido inverso:
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+sorted(shopping, reverse=True)
+print(sorted(shopping, reverse=True))
+print(shopping)
+
+#BONITA APLICACION DE ESTAS PROPIEDADES - CORRECION DE FECHA
+input_date = '12/31/20'
+
+splitted_date = input_date.split('/')
+day = splitted_date[1]
+month = splitted_date[0]
+year = '20' + splitted_date[2]
+
+output_date = '-'.join([day, month, year])
+print(output_date)
+
+#ITERAR USANDO ENUMERACIÓN y sin usarla
+shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+
+for i, product in enumerate(shopping):
+    print(i + 1, product)
+
+for product in shopping:
+    print(product)
+
+#ITERAR SOBRE MÚLTIPLES LISTAS - iterar sobre múltiples listas en paralelo utilizando la función zip():
+
+shopping = ['Agua', 'Aceite', 'Arroz']
+details = ['mineral natural', 'de oliva virgen', 'basmati']
+
+for product, detail in zip(shopping, details):
+    print(product, detail)
+
+print (list(zip(shopping, details)))
+ #crea una lista con nuevos grupos, con los items integrados
+
+v1 = [4, 3, 8, 1]
+v2 = [9, 2, 7, 3]
+
+for va, vb in zip(v1, v2):
+    print (va * vb)
 
 
 
